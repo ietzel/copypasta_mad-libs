4c7b42b70ce77f6146263b905ca5364391f6e785
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-18 13:39:12 -0400

    Update and rename c_ml.lua to c_ml.cr

---
+++ c_ml.cr
@@ -0,0 +1,12 @@
+puts "Adjective"
+ajective1 = gets
+puts "Noun"
+noun1 = gets
+puts "Past Tense Exclamation"
+past_exclamation1 = gets
+
+function max(adjective1, noun1, past_exclamation1)
+puts "Earlier today I was really " #{adjective1} ", and I saw what I thought to be a blank DVD. I thought, DVDs have a tight hole, they might feel pretty good. So I put my soft " #{noun1} " into the hole of the DVD, and for a few seconds as I started getting harder, it felt pretty good, but then, once I was fully erect, it started being painful. My pp was stuck in the DVD, and I had to break it in half to get if out. It was then when I flipped the broken DVD over and realized that it was not a blank DVD, but a copy of the Pixar movie Up.. Well guys, guess I " #{past_exclamation} " up."
+end
+
+max #{adjective1} #{noun1} #{past_exclamation1}
